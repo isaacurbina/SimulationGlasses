84dd65acda50edc4e9ef1c8ff30c9f7dbfb006cb
Isaac Urbina <isaac.urbina@naranya.com>
2014-06-28 17:12:38 +0800

    url bar changed

--- script.js
+++ script.js
@@ -2,21 +2,33 @@ var blur= 0;
 var increment= 5;
 
 $(document).ready(function() {
+
+	if (getURLParameters("q")!=null) {
+		var url= getURLParameters("q");
+	}
 	
 	$("#go_btn").click(function() {
 		var url= $("#url").val();
-		if (url.indexOf("http://") <= -1) {
-	    	url= "http://"+url;
-	    	$("#url").val(url);
-	    }
-	    $("#iframe").fadeOut(1000);
-		$("#iframe").attr("src", url).delay(1000);
+		changeIframeURL(url);
 		console.log("changing to "+url);
+	});
+
+	$("#go_btn_lp").click(function() {
+		var url= $("#url_lp").val();
+		changeIframeURL(url);
+		console.log("forwarding to viewer with url "+url);
+		document.location.href= "viewer.html?q="+encodeURIComponent(url);
 	});
 
 	$("#url").keyup(function (e) {
 	    if (e.keyCode == 13) {
 	        $("#go_btn").click();
+	    }
+	});
+
+	$("#url_lp").keyup(function (e) {
+	    if (e.keyCode == 13) {
+	        $("#go_btn_lp").click();
 	    }
 	});
 
@@ -54,3 +66,44 @@ $(document).ready(function() {
 	});
 
 });
+
+function getURLParameters(paramName) 
+{
+        var sURL = window.document.URL.toString();  
+    if (sURL.indexOf("?") > 0)
+    {
+       var arrParams = sURL.split("?");         
+       var arrURLParams = arrParams[1].split("&");      
+       var arrParamNames = new Array(arrURLParams.length);
+       var arrParamValues = new Array(arrURLParams.length);     
+       var i = 0;
+       for (i=0;i<arrURLParams.length;i++)
+       {
+        var sParam =  arrURLParams[i].split("=");
+        arrParamNames[i] = sParam[0];
+        if (sParam[1] != "")
+            arrParamValues[i] = unescape(sParam[1]);
+        else
+            arrParamValues[i] = "No Value";
+       }
+
+       for (i=0;i<arrURLParams.length;i++)
+       {
+                if(arrParamNames[i] == paramName){
+            //alert("Param:"+arrParamValues[i]);
+                return arrParamValues[i];
+             }
+       }
+       return null;
+    }
+
+}
+
+function changeIframeURL(url) {
+	if (url.indexOf("http://") <= -1) {
+    	url= "http://"+url;
+    	$("#url").val(url);
+    }
+    $("#iframe").fadeOut(1000);
+	$("#iframe").attr("src", url).delay(1000);
+}
